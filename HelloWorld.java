2d606e6c76382c1a1126c05129bccc1feb764eb1
MadsDamiri87 <madsc.damiri@gmail.com>
2025-09-20 15:28:52 +0200

    Maybe its resolved

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Ændring fra VSCode, som skaber konflikt!");
-        System.out.println("Hello from IntelliJ");
+      System.out.println("Ændring fra IntelliJ, som skaber konflikt!");
     }
 
 
