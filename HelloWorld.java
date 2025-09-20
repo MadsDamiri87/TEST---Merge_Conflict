9350db16996eab83896b0a8f03ee7c8cab852556
MadsDamiri87 <madsc.damiri@gmail.com>
2025-09-20 14:56:36 +0200

    Commit. - Only HelloWorld and no .gitignore or other "files"

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello from VSCode!");
+        System.out.println("Hello from IntelliJ!");
     }
 
 
